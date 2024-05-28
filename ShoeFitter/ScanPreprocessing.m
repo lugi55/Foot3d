9cb6a410f9813930e9116c79e3b5e25002c1f3e6
Frieder <Frieder.Luginsland@web.de>
2024-05-28 17:08:38 +0200

    automatic left right foot reflection

--- ShoeFitter/ScanPreprocessing.m
+++ ShoeFitter/ScanPreprocessing.m
@@ -8,7 +8,7 @@ offset = [0.07      ,0;
           -0.07     ,0.04;
           0         ,0.04];
 
-Folder = 'Scans/0001/';
+Folder = 'Scans/0015l/';
 listDir = dir([Folder,'*ply']);
 
 for i=1:length(listDir)
@@ -26,6 +26,9 @@ for i=1:length(listDir)
     ptCloud = pointCloud(ptCloud.Location - [ptMean(1:2),0]);
     ptCloud = pointCloud(ptCloud.Location*rotz(180+str2double(listDir(i).name(1:3))));
     ptCloud = pointCloud(ptCloud.Location + [offset(i,:),0]);
+    if(contains(Folder,'l'))
+        ptCloud = pointCloud(ptCloud.Location.*[1 -1 1]); %Left to Right Foot
+    end
     scatter3(ptCloud.Location(:,1),ptCloud.Location(:,2),ptCloud.Location(:,3),'.')
     xlabel("x")
     ylabel("y")
@@ -34,6 +37,7 @@ for i=1:length(listDir)
     hold on
 
     pt{i} = double(ptCloud.Location);
+
 end
 
 save([Folder,'pt.mat'],'pt')
